ccd150d6fbf1f3bfe6984cf728cfbd0ec77a548c
venkadesh-chellamuthu <venkadesh.008@gmail.com>
2021-12-15 08:32:35 +0530

    qustion changed

--- src/components/Qustion.js
+++ src/components/Qustion.js
@@ -1,51 +1,51 @@
 export default [
 {
-    question:"1st quistion",
+    question:"1st qustion",
     options:["option1","option2","option3","option4"],
     answer:1,
 },
 {
-    question:"2st quistion",
+    question:"2nd qustion",
     options:["option1","option2","option3","option4"],
     answer:3,
 },
 {
-    question:"3st quistion",
+    question:"3rd qustion",
     options:["option1","option2","option3","option4"],
     answer:2,
 },
 {
-    question:"4st quistion",
+    question:"4th qustion",
     options:["option1","option2","option3","option4"],
     answer:3,
 },
 {
-    question:"5st quistion",
+    question:"5th qustion",
     options:["option1","option2","option3","option4"],
     answer:0,
 },
 {
-    question:"6st quistion",
+    question:"6th qustion",
     options:["option1","option2","option3","option4"],
     answer:1,
 },
 {
-    question:"7st quistion",
+    question:"7th qustion",
     options:["option1","option2","option3","option4"],
     answer:3,
 },
 {
-    question:"8st quistion",
+    question:"8th qustion",
     options:["option1","option2","option3","option4"],
     answer:2,
 },
 {
-    question:"9st quistion",
+    question:"9th qustion",
     options:["option1","option2","option3","option4"],
     answer:3,
 },
 {
-    question:"10st quistion",
+    question:"10th qustion",
     options:["option1","option2","option3","option4"],
     answer:0,
 }
